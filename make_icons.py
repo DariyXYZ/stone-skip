583db83722b0ac6b6300c89245f154fd8c1c551e
dariy.n <dariy.n@IND-288>
2026-05-12 23:15:15 +0300

    repo: keep only deployable pwa files

--- make_icons.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-src = Image.open('logo.jpg').convert('RGBA')
-
-for size in [192, 512]:
-    img = src.resize((size, size), Image.LANCZOS)
-    img.save(f'icon-{size}.png')
-    print(f'icon-{size}.png saved')
